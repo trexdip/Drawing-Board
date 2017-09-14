56c7111c6c4aaa725f04c8d557c013c1cd88fcc3
trexdip <trexdip@hotmail.com>
2017-09-14 12:41:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var canvas= document.createElement('canvas');
+canvas.setAttribute('id','canvas');
+document.body.appendChild(canvas);
+canvas.height= 600;
+canvas.width= 800;
+canvas.style.border= "1px solid";
+
+var c= canvas.getContext('2d');
