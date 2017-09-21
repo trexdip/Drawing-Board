ebdec5347cedadf13980d7565cbe658aeef138ad
trexdip <trexdip@hotmail.com>
2017-09-21 13:34:01 -0700

    fifth commit

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ document.body.appendChild(canvas);
 canvas.height= 600;
 canvas.width= 800;
 canvas.style.border= "1px solid";
-
 var c= canvas.getContext('2d');
+c.fillStyle="#bcbbbb";
+c.fillRect(10,20,100,100)
+c.fillRect(110,120,50,50)
+c.fillRect(160,170,30,30)
+c.fillRect(190,200,20,20)
+c.fillRect(210,220,10,10)
+c.fillRect(220,230,5,5)
+c.fillRect(225,235,4,19)
